b04fa7014f0bbe355d9937deddcded9298039bc6
Kriti Shah <shahk.gos@gmail.com>
2021-04-24 21:34:16 -0400

    Determined which findings were important or secondary

--- EDA script.R
+++ EDA script.R
@@ -46,25 +46,106 @@ ggplot(patients_data) +
   geom_bar(mapping = aes(Stay))
 
 ggplot(patients_data) +
-  geom_bar(mapping = aes(Stay))
+  geom_bar(mapping = aes(stay))
   
   ##Thorough univariate investigation of important predictor variable(s) - ones either believed to be important (domain knowledge) or hypothesized to be important. 
 
 
   ##Interesting/important relationships between response variable(s) and predictor variables.
 
-mosaic(Stay ~ Age + `BedGrade`, data = patients_data)
+# mosaic(stay ~ age + bed_grade, data = patients_data)
+# 
+# png(file = "data/mosaic.png", width = 9, height = 9, units = "in", res = 140)
+# mosaic(stay ~ age, patients_data, rot_labels=c(90,90,0,0)
+# )
+# dev.off()
+
+# IMPORTANT: As age increases, length of stay increases
+ggplot(patients_data, aes(x = age, fill = stay)) +
+  geom_bar(position = "fill")
+
+
+# mosaic(stay ~ hospital_type_code, patients_data)
+# 
+# ggplot(patients_data, aes(x = hospital_type_code, fill = stay)) +
+#   geom_bar(position = "fill")
+
+# mosaic(stay ~ city_code_hospital, patients_data)
+# 
+# ggplot(patients_data, aes(x = city_code_hospital, fill = stay)) +
+#   geom_bar(position = "fill")
+
+# mosaic(stay ~ hospital_region_code, patients_data)
+
+
+
+# mosaic(stay ~ department, patients_data)
+
+# Important: There is a difference in length of stay across departments; surgery has the longest
+ggplot(patients_data, aes(x = department, fill = stay)) +
+  geom_bar(position = "fill") +
+  coord_flip()
+
+# mosaic(stay ~ ward_type, patients_data)
+
+
+# mosaic(stay ~ ward_facility_code, patients_data)
+# 
+# ggplot(patients_data, aes(x = ward_facility_code, fill = stay)) +
+#   geom_bar(position = "fill")
+
+# mosaic(stay ~ bed_grade, patients_data)
+# 
+# ggplot(patients_data, aes(x = bed_grade, fill = stay)) +
+#   geom_bar(position = "fill")
+
+# mosaic(stay ~ type_of_admission, patients_data)
+
+# Trauma patients have the longest stay
+ggplot(patients_data, aes(x = type_of_admission, fill = stay)) +
+  geom_bar(position = "fill")
+# 
+# mosaic(stay ~ severity_of_illness, patients_data)
+
+# Extreme illness has longest stay
+ggplot(patients_data, aes(x = severity_of_illness, fill = stay)) +
+  geom_bar(position = "fill")
+
+# mosaic(stay ~ hospital_code, patients_data)
+# 
+# ggplot(patients_data, aes(x = hospital_code, fill = stay)) +
+#   geom_bar(position = "fill")
+
+# ggplot(patients_data, mapping = aes(x = stay, y = available_extra_rooms_in_hospital)) +
+#   geom_boxplot()
+
+# ggplot(patients_data, mapping = aes(x = stay, y = admission_deposit)) +
+#   geom_boxplot()
+
+# Important: The longer you stay, the more visitors you have
+ggplot(patients_data, mapping = aes(x = stay, y = visitors_with_patient)) +
+  geom_boxplot()
+
+# ggplot(patients_data, mapping = aes(x = stay, y = city_code_patient)) +
+#   geom_boxplot()
+
 
 #Hospital_type_Code, Hospital_region_code, `Available Extra Rooms in Hospital`, Department, Ward_Type**
 #
 
 
 
-   ##Interesting/important relationships among predictor variables.
+#    ##Interesting/important relationships among predictor variables.
+# 
+# mosaic( ~ hospital_code + hospital_type_code, patients_data)
+
+ggplot(patients_data, mapping = aes(visitors_with_patient, available_extra_rooms_in_hospital)) +
+  geom_point() 
 
 
 
-
+# ggplot(patients_data, mapping = aes(admission_deposit, available_extra_rooms_in_hospital)) +
+#   geom_point()
 
 
 
@@ -75,3 +156,13 @@ mosaic(Stay ~ Age + `BedGrade`, data = patients_data)
 
 
   ##Findings that don't seem interesting or important, but show some potential. 
+
+
+# Important: There is no significant difference in length of stay between regions
+ggplot(patients_data, aes(x = hospital_region_code, fill = stay)) +
+  geom_bar(position = "fill")
+
+
+# Important: There is a difference in length of stay across ward type
+ggplot(patients_data, aes(x = ward_type, fill = stay)) +
+  geom_bar(position = "fill")
