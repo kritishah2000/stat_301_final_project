70b611e39a6370bcb62be3e3ab8c4f6b77c07257
Ang Jang <angiejang2001@Angs-MacBook-Pro.local>
2021-04-25 12:59:25 -0500

    adding initial overview

--- EDA script.R
+++ EDA script.R
@@ -22,6 +22,27 @@ skim_without_charts(patients_data)
   ##source(s) and any relevant information concerning how the data was collected/formed
    ##Number of observations (n), number of features (p), analysis of missingness (amount and patterns)
 
+Our data was taken from a Kaggle dataset called [AV: Healthcare Analytics II](https://www.kaggle.com/nehaprabhavalkar/av-healthcare-analytics-ii) published by Neha Prabhavlakar in 2020. The data was compiled by Analytics Vidhya for a Kaggle competition exploring the theme of healthcare analytics and the application of machine learning on healthcare case studies. 
+
+In general, the dataset contains information on various factors that may influence the length of stay for a patient during the COVID-19 pandemic. Ultimately, using these variables, we were intending to train models to predict the length of stay by relating to the response variable (`stay` -- number of stay days by the patient) with the following 13 predictor variables:
+* `hospital_code`: unique code for hospital
+* `hospital_type_code`: unique code for type of hospital
+* `hospital_region_code`: region code of hospital
+* `available_extra_rooms_in_hospital`: number of extra rooms available in the hospital
+* `department`: department overlooking the case
+* `ward_type`: code for the ward type
+* `ward_facility_code`: code for the ward facility
+* `bed_grade`: condition of bed in the ward
+* `visitors_with_patient`: number of visitors with patient
+* `age`: age of the patient
+* `severity_of_illness`: severity of illness recorded at the time of admission
+* `type_of_admission`: admission type registered by the hospital
+* `admission_deposit`: deposit at the admission time
+
+In total, there are 318438 rows of data. Upon reading in the data, we can see that there are only two variables with missing data: `bed_grade` and `city_code_patient`. Because `city_code_patient` is a unique identifier for each patient that would not reveal interesting relationships, we decided to exclude this variable from our analysis. However, we decided to keep `bed_grade`, so we planned to filter for  rows of data that were not missing `bed_grade` (excluding the 113 values of missing data).
+
+Lastly, for our models, we also decided to exclude `case_id` and `patientid` because these were unique identifiers for each patient that would not show important relationships in the data.
+
 #Essential Findings (Important or Interesting)
   ##Thorough univariate investigation of response variable(s)
 
